ebb6db139d9f0f6e62bbb71dbdc3b37bad5e564b
abhishek-0079 <abhishekprajapati0079@gmail.com>
2024-10-09 12:03:52 +0530

    Day5

---
+++ Day5/script.js
@@ -0,0 +1,11 @@
+"use strict" // iski wajah se variable declaration strictly follow hoga
+function display(){
+    console.log("Welcome to the website")
+}
+display()
+ let a=1;
+ const b=10;
+
+ console.log(a)
+ console.log(b)
+
