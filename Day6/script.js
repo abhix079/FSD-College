5e41e3255d02d066672d42de9e975fc36f6c760b
abhishek-0079 <abhishekprajapati0079@gmail.com>
2024-10-14 12:50:09 +0530

    Day6

--- Day6/script.js
+++ Day6/script.js
@@ -1,16 +1,39 @@
+// // let a = () => {
+// //   console.log("Arrow Function");
+// // };
+
+// // let b = () => console.log("Arrow Function");
+// // a();
+// // b();
+
+// // let add = (a, b) => {
+// //   return a + b;
+// // };
+// // let res = add(5, 8);
+
+// // console.log(res);
+// //  function display(){
+
+// // let p = document.querySelectorAll('p');
+// // p[0].innerHTML ="I am in 1st parapgraph";
+// // p[0].style.backgroundColor = "red";
+// // p[1].innerHTML ="I am in  2nd parapgraph";
+// // p[1].style.backgroundColor = "green";
+
+// // }
+
+// function change(){
+
+//     let p = document.querySelector('h1');
+//     p.textContent ="Changed Heading";
+//     let a1 = document.querySelector('a');
+//     a1.setAttribute("href","https://aktu.ac.in");
+//     a1.innerHTML ="aktu website";
+// }
+
+// let addContent=() =>{
+//     const newDiv =document.createElement('div');
+//     newDiv.textContent = "THis is new div";
 
 
-let a= ()=>{
-    console.log("Arrow Function");
-};
-
-let b=()=>console.log("Arrow Function");
-a();
-b();
-
-let add =(a,b) =>{
-    return a+b;
-};
-let res=add(5,8);
-
-console.log(res);
+// };
