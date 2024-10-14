4bfadfab45252061bc0a7a942899598a57e6ae42
abhishek-0079 <abhishekprajapati0079@gmail.com>
2024-10-14 11:48:13 +0530

    Day6

---
+++ Day6/script.js
@@ -0,0 +1,16 @@
+
+
+let a= ()=>{
+    console.log("Arrow Function");
+};
+
+let b=()=>console.log("Arrow Function");
+a();
+b();
+
+let add =(a,b) =>{
+    return a+b;
+};
+let res=add(5,8);
+
+console.log(res);
