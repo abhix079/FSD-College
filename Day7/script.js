eacdefdc7e65425567987f2855fb9f2a1efdf5de
abhishek-0079 <abhishekprajapati0079@gmail.com>
2024-10-16 12:30:44 +0530

    Day7

--- Day7/script.js
+++ Day7/script.js
@@ -1,14 +1,51 @@
-let num = [1,2,3,4,5,6,7,8,9,10];
+// let num = [1,2,3,4,5,6,7,8,9,10];
 
-//MAP METHOD
-let newnum = num.map(x=>x*2);
-console.log(newnum);
+// //MAP METHOD
+// let newnum = num.map(x=>x*2);
+// console.log(newnum);
 
 
-//Filter method
-let arrfilter = num.filter(x => (x%2)==0);
-console.log(arrfilter);
+// //Filter method
+// let arrfilter = num.filter(x => (x%2)==0);
+// console.log(arrfilter);
 
-// reduce method
+// // reduce method
+//     //SYntax :     arrName.reduce((accumulator,currentValue) => accumulator+currentValue ,initialValue)
 
-let res =num.reduce()
+// let res =num.reduce((x,y)=>x+y,0);
+// console.log(res);
+
+// //product 
+
+// let pro = num.reduce ((x,y)=>x*y,1);
+// console.log(pro);
+
+// //Creating objects
+
+// const students= [
+//     {name:"Alice" ,score:50},
+//     {name:"Bob" ,score:65},
+//     {name:"Charlie" ,score:80},
+//     {name:"David" ,score:45},
+//     {name:"Rahul" ,score:50},
+
+// ];
+// let scores= students.map(x=>x.score);
+// console.log(scores);
+
+// let result = scores.reduce((x,y)=>x+y,0);
+// console.log(result);
+
+// let mulScore  = students.map(x => x.score*2);
+// console.log(mulScore);
+
+// let mulRes = mulScore.reduce((x,y)=>x+y,0);
+// console.log(mulRes);
+
+// let fil = scores.filter(x =>x>60);
+// let sum  = fil.reduce((x,y)=>x+y,0);
+// console.log(sum);
+
+
+let num = [1,2,3,4,5,6,7,8,9];
+let arr  = num.forEach(x =>console.log(x));
