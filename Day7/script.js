0f23287c944ed77ef99c3f25a6c0d2f90c66a695
abhishek-0079 <abhishekprajapati0079@gmail.com>
2024-10-16 13:00:28 +0530

    Day7

--- Day7/script.js
+++ Day7/script.js
@@ -4,7 +4,6 @@
 // let newnum = num.map(x=>x*2);
 // console.log(newnum);
 
-
 // //Filter method
 // let arrfilter = num.filter(x => (x%2)==0);
 // console.log(arrfilter);
@@ -15,7 +14,7 @@
 // let res =num.reduce((x,y)=>x+y,0);
 // console.log(res);
 
-// //product 
+// //product
 
 // let pro = num.reduce ((x,y)=>x*y,1);
 // console.log(pro);
@@ -46,6 +45,28 @@
 // let sum  = fil.reduce((x,y)=>x+y,0);
 // console.log(sum);
 
+// let num = [1,2,3,4,5,6,7,8,9];
 
-let num = [1,2,3,4,5,6,7,8,9];
-let arr  = num.forEach(x =>console.log(x));
+// let arr  = num.forEach(x =>console.log(x));
+
+//ASYCHRonous functin
+
+// sayHello = () =>{
+//     console.log("I am in  Helo function");
+// };
+
+// console.log("Start");
+// setTimeout(sayHello,3000);
+// console.log("End");
+
+console.log("Start");
+setTimeout(() => {
+  console.log("first task completd");
+  setTimeout(() => {
+    console.log("Second task completd");
+    setTimeout(() => {
+      console.log("third task completed");
+    }, 3000);
+  }, 2000);
+}, 1000);
+console.log("End");
