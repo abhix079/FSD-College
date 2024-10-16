04ebd40e222fba3aff71352e49b81ee5f2842c95
abhishek-0079 <abhishekprajapati0079@gmail.com>
2024-10-16 11:51:39 +0530

    Day7

---
+++ Day7/script.js
@@ -0,0 +1,14 @@
+let num = [1,2,3,4,5,6,7,8,9,10];
+
+//MAP METHOD
+let newnum = num.map(x=>x*2);
+console.log(newnum);
+
+
+//Filter method
+let arrfilter = num.filter(x => (x%2)==0);
+console.log(arrfilter);
+
+// reduce method
+
+let res =num.reduce()
