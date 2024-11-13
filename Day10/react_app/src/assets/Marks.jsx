31592898af084ba56b524deb69ade6b8a051ca87
abhishek-0079 <abhishekprajapati0079@gmail.com>
2024-11-13 13:08:13 +0530

    React

---
+++ Day10/react_app/src/assets/Marks.jsx
@@ -0,0 +1,16 @@
+const Marks =({name,roll,m1,m2,m3})=>{
+    return (
+        <>
+        <div className="mrk">
+            <h1>Marksheet</h1>
+            <h2>Name: {name}</h2>
+            <h2>Roll No.:{roll}</h2>
+            <h2>Semester</h2>
+            <h2></h2>
+            <h2></h2>
+
+            <h2></h2>
+        </div>
+        </>
+    );
+}
