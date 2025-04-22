8b6934fb9de3b9cefacda49b87125b8fb93ac85c
abhix079 <abhishek788k@gmail.com>
2025-04-22 14:36:08 +0530

    random

--- UserApp/Server/config/db.js
+++ UserApp/Server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
 // const url = "mongodb://localhost:27017/userappDB";
-const url = process.env.MONGO_URL;
+const url = process.env.MONGO_URL || ATLAS_URL;
 console.log(url);
 const connectDB = async()=>{
     try{
--- UserApp/Server/controller/userController.js
+++ UserApp/Server/controller/userController.js
@@ -1,79 +1,79 @@
-const User= require("../model/usermodel");
-const getAllUsers= async(req,res)=>{
-        try{
-            const users= await User.find();
-            res.status(200).json(users);
+const User = require("../model/usermodel");
+const getAllUsers = async (req, res) => {
+    try {
+        const users = await User.find();
+        res.status(200).json(users);
 
-        }
-        catch(err){
-            res.status(500).json({
-                message:err.message
-            })
+    }
+    catch (err) {
+        res.status(500).json({
+            message: err.message
+        })
 
 
-        }
+    }
 }
-const getUserByEmail= async(req,res)=>{
-    try{
-       const email = req.params.email;
-       const userEmail = await User.findOne({email});
+const getUserByEmail = async (req, res) => {
+    try {
+        const email = req.params.email;
+        const userEmail = await User.findOne({ email });
         res.status(200).json(userEmail);
 
     }
-    catch(err){
+    catch (err) {
         res.status(500).json({
-            message:err.message
+            message: err.message
         })
 
 
     }
 }
-const addUser= async(req,res)=>{
-    try{
-       const {name,email,password,role}= req.body;
-       const newUser= new User({name,email,password,role});
-      const user= await newUser.save();
-      console.log(user,newUser)
+const addUser = async (req, res) => {
+    try {
+        const { name, email, password, role } = req.body;
+        const newUser = new User({ name, email, password, role });
+        const user = await newUser.save();
+        console.log(user, newUser)
         res.status(200).json(user);
 
     }
-    catch(err){
+    catch (err) {
         res.status(500).json({
-            message:err.message
+            message: err.message
         })
 
 
     }
 }
-const updateUser= async(req,res)=>{
-    try{
-      const email = req.params.email;
-      const {name,password,role}= req.body;
-      const updateUser= await User.findOneAndUpdate({email},{name,password,role},{new:true});
+const updateUser = async (req, res) => {
+    try {
+        const email = req.params.email;
+        const { name, password, role } = req.body;
+        const updateUser = await User.findOneAndUpdate({ email }, { name, password, role }, { new: true });
         res.status(200).json(updateUser);
 
     }
-    catch(err){
+    catch (err) {
         res.status(500).json({
-            message:err.message
+            message: err.message
         })
 
 
     }
 }
-const deleteUser= async(req,res)=>{
-    try{
-      const email = req.params.email;
-      const deleteUser= await User.deleteOne({email});
+const deleteUser = async (req, res) => {
+    try {
+        const email = req.params.email;
+        const deleteUser = await User.deleteOne({ email });
         res.status(200).json(deleteUser);
 
     }
-    catch(err){
+    catch (err) {
         res.status(500).json({
-            message:err.message
+            message: err.message
         })
 
 
     }
 }
-module.exports = {getAllUsers,getUserByEmail,addUser,updateUser,deleteUser};
+module.exports = { getAllUsers, getUserByEmail, addUser, updateUser, deleteUser };
